8359d7b38ae98b0654826058413324325999eda9
Paolo Roth <iamoctod@gmail.com>
2019-07-23 17:51:35 +0200

    feat: adds nullable and typescript types

--- src/primitives.ts
+++ src/primitives.ts
@@ -1,5 +1,14 @@
 import { create, Type } from "./type";
 
+export type Any<T = null> = Type<T>;
+export type Bool = Type<boolean>;
+export type Fn = Type<(...args: any[]) => any>;
+export type Obj = Type<object>;
+export type Null = Type<null>;
+export type Num = Type<number>;
+export type Str = Type<string>;
+export type Undefined = Type<undefined>;
+
 export function Any<T = null>(value: T = null as any): Type<T> {
   return create("any", value);
 }
@@ -22,6 +31,15 @@ export function Obj(value: object = {}): Type<object> {
 
 export function Null(): Type<null> {
   return create("null", null, value => value === null);
+}
+
+export function Nullable<U>(type: Type<U>): Type<U | null> {
+  const name = ["null", type.type()].join(".");
+  return create(
+    name,
+    type.value(),
+    value => value === null || type.check(value)
+  );
 }
 
 export function Num(value: number = 0): Type<number> {
