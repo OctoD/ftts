8ed08a83b00f78b7a000a6ce349c7022c23b5da0
Paolo Roth <iamoctod@gmail.com>
2019-07-24 11:21:20 +0200

    chore: upd8

--- src/operators/Bool.ts
+++ src/operators/Bool.ts
@@ -1,13 +1,14 @@
+import { create, store } from "../operator";
+import { any, bool, Bool } from "../primitives";
+import { Type } from "../type";
 import { DefaultSigns } from "./DefaultSigns";
-import { create, store, Operator } from "../operator";
-import { bool, any } from "../primitives";
 
 export interface BoolOperators {
-  is: Operator<any, boolean>;
+  is(a: Type<any>, b: Bool): boolean;
 }
 
 export const BoolOperators = <BoolOperators>{};
 
 BoolOperators.is = store(
   create(DefaultSigns.Equals, any, bool, (a, b) => !!a.value() === b.value())
-);
+).get();
--- src/operators/__tests__/bool.spec.ts
+++ src/operators/__tests__/bool.spec.ts
@@ -3,12 +3,8 @@ import { BoolOperators } from "../Bool";
 
 describe(`bool operators`, () => {
   it(`compares truthiness of falsiness of types`, () => {
-    const is = BoolOperators.is.get();
-
-    expect(is(Undefined(), Bool(false))).toBeTruthy();
-
-    expect(is(Bool(true), Bool(false))).toBeFalsy();
-
-    expect(is(Num(), Bool())).toBeFalsy();
+    expect(BoolOperators.is(Undefined(), Bool(false))).toBeTruthy();
+    expect(BoolOperators.is(Bool(true), Bool(false))).toBeFalsy();
+    expect(BoolOperators.is(Num(), Bool())).toBeFalsy();
   });
 });
