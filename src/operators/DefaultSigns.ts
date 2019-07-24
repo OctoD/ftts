4ad2e17b3378c07b81992a25b835140d943b3423
Paolo Roth <iamoctod@gmail.com>
2019-07-24 09:22:06 +0200

    chore: adds signs

--- src/operators/DefaultSigns.ts
+++ src/operators/DefaultSigns.ts
@@ -1,11 +1,15 @@
 export enum DefaultSigns {
   Decrease = "--",
   Divide = "/",
+  DoubleQuestionMark = "??",
   Equals = "==",
   Increase = "++",
   Minus = "-",
   Multiply = "*",
   Plus = "+",
   Pow = "**",
+  QuestionMark = "?",
   Root = "^"
 }
+
+export default DefaultSigns;
