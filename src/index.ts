fded9333ea0ec50001ef7c3be16364241a1d05b8
Paolo Roth <iamoctod@gmail.com>
2019-09-02 16:17:26 +0200

    chore: dumps operators

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import * as arr from "./arr";
 import * as int from "./int";
-import * as operator from "./operator";
-import * as operators from "./operators";
 import * as primitives from "./primitives";
 import * as struct from "./struct";
 import * as trait from "./trait";
 import * as type from "./type";
 
-export { arr, int, operator, operators, primitives, struct, trait, type };
+export { arr, int, primitives, struct, trait, type };
 
 export default {
   arr,
   int,
-  operator,
-  operators,
   primitives,
   struct,
   trait,
